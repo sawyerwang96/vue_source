14beb088b1b9764259fb32c191f2c2424277f3f2
sawyerwang <sawyerwang96@163.com>
2022-03-17 21:00:19 +0800

    dom-diff

--- src/vdom/patch.js
+++ src/vdom/patch.js
@@ -46,7 +46,7 @@ export function patch(oldVnode, vnode) {
       updateChildren(el, oldChildren, newChildren);
     } else if (newChildren.length) {
       // 新的有子节点
-      for (let i = 0 ; i < newChildren.length; i++) {
+      for (let i = 0; i < newChildren.length; i++) {
         let child = newChildren[i];
         el.appendChild(createElm(child));
       }
@@ -120,23 +120,86 @@ function updateChildren(parent, oldChildren, newChildren) {
   let newEndIndex = newChildren.length - 1;
   let newEndVnode = newChildren[newEndIndex];
 
+  const makeIndexByKey = (children) => {
+    let map = {};
+    children.forEach((item, index) => {
+      if (item.key) {
+        map[item.key] = index; // 根据key创建一个映射表
+      }
+    });
+    return map;
+  };
+  let map = makeIndexByKey(oldChildren);
+
   // 在对比过程中，新老虚拟就节点只要有一方循环完毕就结束了（指针重合）
-  while(oldStartIndex <= oldEndIndex && newStartIndex <= newEndIndex) {
-    if (isSameVnode(oldStartVnode, newStartVnode)) {
+  while (oldStartIndex <= oldEndIndex && newStartIndex <= newEndIndex) {
+    if (!oldStartVnode) {
+      // 在旧节点start指针向后移动的过程中可能会碰到undefined（暴力对比中会置空）
+      oldStartVnode = oldChildren[++oldStartVnode];
+    } else if (!oldEndVnode) {
+      oldEndVnode = oldChildren[--oldEndIndex];
+    } else if (isSameVnode(oldStartVnode, newStartVnode)) {
       // 如果同一个节点 就需要对比这个元素的属性
       patch(oldStartVnode, newStartVnode);
       oldStartVnode = oldChildren[++oldStartIndex];
+      newStartVnode = newChildren[++newStartIndex];
+    } else if (isSameVnode(oldEndVnode, newEndVnode)) {
+      patch(oldEndVnode, newEndVnode);
+      oldStartVnode = oldChildren[--oldEndIndex];
+      newStartVnode = newChildren[--newEndIndex];
+    } else if (isSameVnode(oldStartVnode, newEndVnode)) {
+      // 头移尾 （涉及 倒序变正序）
+      patch(oldStartVnode, newEndVnode);
+      parent.insertBefore(oldStartVnode.el, oldEndVnode.el.nextSibling);
+      oldStartVnode = oldChildren[++oldStartIndex];
+      newEndVnode = newChildren[--newEndIndex];
+    } else if (isSameVnode(oldEndVnode, newStartVnode)) {
+      patch(oldEndVnode, newStartVnode);
+      parent.insertBefore(oldEndVnode.el, oldStartVnode.el);
+      oldEndVnode = oldChildren[--oldEndIndex];
+      newStartIndex = newChildren[++newStartIndex];
+    } else {
+      // 暴力对比 
+      // 查找规则：先取出新的第一个虚拟节点 通过key来查找是否存在
+      // 现根据老节点的key 做一个映射表 拿新的虚拟节点去映射表中查找 
+      // 如果可以找到 则进行移动操作 移到统一指针的前面位置
+      // 如果找不到则直接将元素插入即可
+      let moveIndex = map[newStartVnode.key];
+      if (!moveIndex) { // 不需要复用
+        parent.insertBefore(createElm(newStartVnode), oldStartVnode.el);
+      } else {
+        // 在映射表中查找到了，则直接将元素移走，并且将当前位置置空
+        let moveVnode = oldChildren[moveIndex]; // 要移动的元素
+        oldChildren[moveIndex] = undefined;
+        parent.insertBefore(moveVnode.el, oldStartVnode.el);
+        patch(moveVnode, newStartVnode); // 比对子元素是否一致
+      }
       newStartVnode = newChildren[++newStartIndex];
     }
   }
 
   if (newStartIndex <= newEndIndex) {
     for (let i = newStartIndex; i < newEndIndex; i++) {
-      // 新增子节点
-      parent.appendChild(createElm(newChildren[i]));
+      // 新增子节点 有可能在向后插入 也有可能向前插入
+      let el = newChildren[newChildren + 1] == null ? null : newChildren[newEndIndex + 1].el;
+      parent.insertBefore(createEle(newChildren[i]), el); // 最前面插入 或者 最后面插入
+      // if (flag == null) {
+      //   parent.insertBefore(createEle(newChildren[i]), null);
+      // } else {
+      //   parent.insertBefore(createEle(newChildren[i]), flag);
+      // }
+      // parent.appendChild(createElm(newChildren[i]));
     }
   }
 
+  if (oldStartIndex <= oldEndIndex) { // 删除旧节点中多余的节点
+    for (let i = oldStartIndex; i <= oldEndIndex; i++) {
+      let child = oldChildren[i];
+      if (child !== undefined) {
+        parent.removeChild(child.el);
+      }
+    }
+  }
 
 }
 
